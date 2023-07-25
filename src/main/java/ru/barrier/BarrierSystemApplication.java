e0df6c6d3e9939865b23aee862c2a8ce36d7f5ed
marat <maratgareev97@gmail.com>
2023-07-25 21:55:51 +0300

    Initial commit

---
+++ src/main/java/ru/barrier/BarrierSystemApplication.java
@@ -0,0 +1,11 @@
+package ru.barrier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarrierSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BarrierSystemApplication.class);
+    }
+}
